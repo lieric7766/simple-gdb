2f094a879cb7c97547ed3a01dbe149f21a58c6f3
lishihong <lieric7766@gmail.com>
2018-03-14 23:55:01 +0800

    add pnext.py

---
+++ pnext.py
@@ -0,0 +1,37 @@
+import gdb
+
+class pnext(gdb.Command):
+    def __init__(self):
+        super(self.__class__, self).__init__("pnext", gdb.COMMAND_USER)
+
+    def invoke(self, args, from_tty):
+        argv = gdb.string_to_argv(args)
+        point = gdb.parse_and_eval(argv[0])
+
+        if len(argv) > 3:
+            raise gdb.GdbError('Usage: pnext [head] [next] [length]')
+
+        next_p = 'next'
+        if len(argv) > 1:
+            next_p = argv[1]
+            try:
+                check = point[next_p].dereference()
+            except gdb.error as e:
+                print("Oops!  ", e)
+                return
+
+        count = 0
+        if len(argv) > 2:
+            try:
+                count = int(argv[2])
+            except ValueError:
+                print("Oops!  That was no valid number", argv[2])
+                return
+
+        while point:
+            if count == 0:
+                break
+            print(point.dereference())
+            point = point[next_p]
+            count -= 1
+pnext()
